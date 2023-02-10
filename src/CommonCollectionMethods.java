f9637fa833c8411f9ffd03f4f9480e6f6dee4bed
elmehdi1212 <elmehdioubari@gmail.com>
2023-02-10 23:23:11 +0100

    Common methods of collection in java

--- src/CommonCollectionMethods.java
+++ src/CommonCollectionMethods.java
@@ -1,22 +1,23 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 public class CommonCollectionMethods {
     public static void main(String[] args) {
         // all method of collection in java
-         Collection<String> coll=new ArrayList<>();
-         coll.add("sam");
+         Collection<String> coll= Arrays.asList("sam,sara,mohamed"); // immutable
+      /*   coll.add("sam");
          coll.add("sara");
-         coll.add("mohamed");
+         coll.add("mohamed");*/
          System.out.println(coll.remove("sara"));     // true
          System.out.println(coll);                       // [sam]
          System.out.println(coll.isEmpty());             // false
          System.out.println(coll.size());                // 1
          System.out.println(coll.contains("sam"));       // true
          System.out.println(coll.removeIf(s->s.startsWith("s"))); //true
-         coll.forEach( name-> System.out.println(name));   // [mohamed]
+         coll.forEach( name-> System.out.println(name));   // [mohamed] // lambda exprience
          coll.clear();
-         coll.forEach(System.out::println);  // nothing
+         coll.forEach(System.out::println);    // nothing   // method reference
 
     }
 }
